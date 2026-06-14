b8ab27c7e3010d88e04aaed8a2f54cf0b29fc2d0
Syuryveiga <veiga.yury@gmail.com>
2026-06-13 21:24:49 -0300

    chore: add Google Analytics tag to Experiences page

--- src/pages/Experiences.tsx
+++ src/pages/Experiences.tsx
@@ -135,13 +135,17 @@ export default function Experiences() {
             }
           })}
         </script>
+        {/* Google tag (gtag.js) */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18075082892"></script>
-        <script>{`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'AW-18075082892');
-        `}</script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'AW-18075082892');
+          `}
+        </script>
       </Helmet>
 
       <Header />
